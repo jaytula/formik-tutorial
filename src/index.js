040bcca1a785997b4475229a843fc1a65eb5035b
Jay T <jaytula@gmail.com>
2020-06-09 00:06:28 -0700

    Add SignupForm with email field

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { useFormik } from 'formik';
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor='email'>Email Address</label>
+      <input
+        id='email'
+        name='email'
+        type='email'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type='submit'>Submit</button>
+    </form>
+  );
+};
+
+const App = () => (
+  <div>
+    <SignupForm />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
